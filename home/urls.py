1296a83e090d9b10872fa78b1dd9461bf9839282
GauriNevase <321gauri0031@dbit.in>
2023-04-07 19:16:55 +0530

    chatbot diary connection with afterlogin

--- home/urls.py
+++ home/urls.py
@@ -1,13 +1,18 @@
 from django.urls import path
 from home import views
-from blog import views as cv
-
+from blog import views as bv
+from diary import views as dv
+from chatbot import views as cv
 
 urlpatterns = [
-    path('', views.index, name="index"),
+    path('index/', views.index, name="index"),
     path('community/', views.index2, name="community"),
-    path('login/',views.login ,name="login"),
-    path('afterlogin/',views.afterlogin,name="aftelogin"),
-    path('blog/', cv.PostList.as_view(), name='blog_home'),
-    path('<slug:slug>/', cv.PostDetail.as_view(), name='blog_post_detail')
+    path('',views.login ,name="login"),
+    path('',views.afterlogin,name="aftelogin"),
+    path('blog/', bv.PostList.as_view(), name='blog_home'),
+    path('<slug:slug>/', bv.PostDetail.as_view(), name='blog_post_detail'),
+    path('diary/',dv.index,name='diary_home'),
+    path('add/', dv.add,name='add'),
+    path('chatbot/',cv.index,name='index'),
+    path('getResponse',cv.getResponse,name='getResponse')
 ]
