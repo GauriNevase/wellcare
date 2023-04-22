110931e99af501e37608f4517ee9cce01b2b23b2
GauriNevase <321gauri0031@dbit.in>
2023-04-22 10:35:28 +0530

    diary changes

--- diary/admin.py
+++ diary/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Diary_entry
+from .models import *
 
 admin.site.register(Diary_entry)
+admin.site.register(Diary)
 
 # Register your models here.
--- diary/views.py
+++ diary/views.py
@@ -1,22 +1,17 @@
 from django.shortcuts import render,redirect
-from .models import Diary_entry
+from .models import *
 from .forms import EntryForm
 
 def index(request):
-    entries = Diary_entry.objects.order_by('-date_posted')
+    entries = Diary.objects.filter(user=request.user)
     context = {'entries' : entries}
     return render(request, 'diary/index.html',context)
 
 def add(request):
     if request.method == 'POST':
-        form=EntryForm(request.POST)
-
-        if form.is_valid():
-            form.save()
-            return redirect('diary_home')
-        
-    else:
-        form=EntryForm()
-
-    context = {'form' : form}
-    return render(request, 'diary/add.html',context)
+        note = request.POST.get('comment')
+        print(note)
+        obj = Diary(user=request.user,context=note)
+        obj.save()
+        redirect('diary_home')
+    return render(request, 'diary/add.html')
